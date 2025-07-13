af9727f8a73aeed6fece078c5b445559ec2e7b83
Prakash R <git@prakashrx.com>
2025-07-13 16:57:59 +1000

    cleanup

--- WebUI/Shared/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-// Global usings for common namespaces across the WebUI.Shared project
-global using System;
-global using System.Collections.Generic;
-global using System.Text.Json;
-global using System.Text.Json.Serialization;
-global using System.Threading.Tasks; 
